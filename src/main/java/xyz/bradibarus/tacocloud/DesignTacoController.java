f1657d97112048faeeeeaafba54c6a1d96b8ae4b
bradibor <just1bomb@gmail.com>
2019-11-16 11:14:55 +0300

    initial

---
+++ src/main/java/xyz/bradibarus/tacocloud/DesignTacoController.java
@@ -0,0 +1,59 @@
+package xyz.bradibarus.tacocloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.bradibarus.tacocloud.model.Ingredient;
+import xyz.bradibarus.tacocloud.model.Ingredient.Type;
+import xyz.bradibarus.tacocloud.model.Taco;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+        Map<Type, List<Ingredient>> ingredientsByType = ingredients.stream()
+                .collect(groupingBy(Ingredient::getType, Collectors.toList()));
+        Arrays.stream(Type.values())
+                .forEach(type-> model.addAttribute(type.toString().toLowerCase(), ingredientsByType.getOrDefault(type, Collections.emptyList())));
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @PostMapping
+    public String design(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+}
