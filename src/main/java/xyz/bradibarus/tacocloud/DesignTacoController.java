46af0e7448a244d30432a7188ed4224426f204ac
bradibor <just1bomb@gmail.com>
2019-12-05 00:37:47 +0300

    ch2

--- src/main/java/xyz/bradibarus/tacocloud/DesignTacoController.java
+++ src/main/java/xyz/bradibarus/tacocloud/DesignTacoController.java
@@ -1,5 +1,6 @@
 package xyz.bradibarus.tacocloud;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.bradibarus.tacocloud.model.Ingredient;
 import xyz.bradibarus.tacocloud.model.Ingredient.Type;
+import xyz.bradibarus.tacocloud.model.IngredientRepository;
 import xyz.bradibarus.tacocloud.model.Taco;
 
 import javax.validation.Valid;
@@ -18,29 +20,32 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.groupingBy;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@RequiredArgsConstructor
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepository;
 
     @ModelAttribute
     public void initIngredients(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
-        Map<Type, List<Ingredient>> ingredientsByType = ingredients.stream()
+//        List<Ingredient> ingredients = Arrays.asList(
+//                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+//                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+//                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+//                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+//                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+//                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+//                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+//                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+//                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+//                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+//        );
+        Map<Type, List<Ingredient>> ingredientsByType = StreamSupport.stream(ingredientRepository.findAll().spliterator(), false)
                 .collect(groupingBy(Ingredient::getType, Collectors.toList()));
         Arrays.stream(Type.values())
                 .forEach(type-> model.addAttribute(type.toString().toLowerCase(), ingredientsByType.getOrDefault(type, Collections.emptyList())));
